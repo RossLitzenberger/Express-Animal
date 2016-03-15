25a9892f5fad438679e51c4e4807bdea6529e18e
Ross <codingclasses.049@gmail.com>
2016-03-15 17:36:11 -0500

    Created the cat crud endpoint

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function (req,res) {
-  res.send('Hello world');
-  // res.json({hello: 'hi'})
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var cats = require('./cats.js')(app);
 
 var server = app.listen(3000, function (){
   console.log('server running at localhost:3000');
